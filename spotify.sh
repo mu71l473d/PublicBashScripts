9db47de4847ed6ff5549d800a0b3c8ca4f2c0817
mu71l473d <judoka_rene@hotmail.com>
2018-08-05 20:22:25 +0000

    Spotify.sh added

---
+++ spotify.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#install spotify from the command line
+
+spotify.sh(){
+	sudo apt-add-repository -y "deb http://repository.spotify.com stable non-free" &&
+	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59 &&
+	sudo apt-get update &&
+	sudo apt-get install spotify-client -y --allow-unauthenticated
+}
