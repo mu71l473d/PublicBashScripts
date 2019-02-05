363c38175b7b02dd0b19a0502da25c09dd22c3d0
mu71l473d <judoka_rene@hotmail.com>
2019-02-05 21:45:40 +0100

    new version

--- defaultinstall.sh
+++ defaultinstall.sh
@@ -12,8 +12,9 @@ defaultinstall.sh () {
 	#installptf;
 	#installwine;
 	#installsumatrapdf;
-	installvmware;
-	installdropbox;
+	#installvmware;
+	#installdropbox;
+	installsignal;
 }
 
 
@@ -57,6 +58,12 @@ installptf () {
 	./ptf
 	#use modules/install_update_all
 	#yes
+}
+
+installsignal () {
+	curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
+	echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+	sudo apt update && sudo apt install signal-desktop
 }
 
 installspotify () {
