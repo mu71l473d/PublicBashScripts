54bf643e748452346282269613fedce378306936
mu71l473d <judoka_rene@hotmail.com>
2020-01-09 15:33:55 +0100

    new version

--- uploadtogithub.sh
+++ uploadtogithub.sh
@@ -1,67 +1,25 @@
 #!/bin/bash
+RED='\033[0;31m'
+NOCOLOR='\033[0m'
 
 update () {
-sudo git pull
-sudo git add *
-sudo git commit -m "new version"
-sudo git push
-echo "Done"
-echo ""
-echo ""
+cd ~/Github
+currentdir=$(pwd)
+
+for D in *; do
+if [ -d "${D}" ]; then
+	cd "$currentdir/${D}"
+	echo
+	echo -e "${RED}$(pwd)${NOCOLOR}"
+	sudo git pull
+	sudo git add *
+	sudo git commit -m "new version"
+	sudo git push
+	cd ..
+fi
+done
 }
-
-
-
-echo "BashScripts"
-cd ~/Github/BashScripts
-update;
-
-echo "PublicBashScripts"
-cd ~/Github/PublicBashScripts
-update;
-
-echo "BanditChallenge"
-cd ~/Github/BanditChallenge
-update;
-
-echo "GreyBlackHatPython"
-cd ~/Github/GreyBlackHatPython
-update;
-
-echo "hacking-taoe"
-cd ~/Github/hacking-taoe
-update;
-
-echo "JavaProjects"
-cd ~/Github/JavaProjects
-update;
-
-echo "PowershellScripts"
-cd ~/Github/PowershellScripts
-update;
-
-echo "PublicPythonScripts"
-cd ~/Github/PublicPythonScripts
-update;
-
-echo "PythonScripts"
-cd ~/Github/PythonScripts
-update;
-
-echo "Books and booknotes"
-cd /media/mu71l473d/Datadisk/books/
-update;
-
-echo "PublicPowershellScripts"
-cd ~/Github/PublicPowerShellScripts
-update;
-
-echo "hackdecamera"
-cd ~/Github/hackcamera
-update;
-
-echo "Kiwashop"
-cd ~/Github/juice-shop
 update;
 
 echo "Done with Uploading"
+
