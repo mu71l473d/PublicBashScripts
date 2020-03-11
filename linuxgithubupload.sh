afbc2a64d856305175e5a21667dcd1ea2bca070c
Rene B <358085@student.saxion.nl>
2020-03-11 17:10:28 +0100

    Update linuxgithubupload.sh

--- linuxgithubupload.sh
+++ linuxgithubupload.sh
@@ -8,17 +8,16 @@ find . -type d -name .git \
  | while read line; \
 	do \
 		cd $line \
+		&& echo "now updating $line"
 		&& update \
 		&& cd -; \
 	done
 }	
 
 update () {
-sudo git pull
-sudo git add *
-sudo git commit -m "new version"
-sudo git push
-echo "Done"
-echo ""
-echo ""
+	sudo git pull
+	sudo git add *
+	sudo git commit -m "new version"
+	sudo git push
+	printf "\nDone\n"
 }
