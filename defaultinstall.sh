d4723b33fb9df5378fc42656556e6272072ae32e
mu71l473d <judoka_rene@hotmail.com>
2020-05-11 12:02:38 +0200

    android studio added

--- defaultinstall.sh
+++ defaultinstall.sh
@@ -8,8 +8,8 @@ defaultinstall.sh () {
 	#update;
 	#installfromapt;
 	#installfromaptgui;
-	installpentest;
-	installmobilepentest;
+	#installpentest;
+	#installmobilepentest;
 	#installicspentest;
 	#installvbguest;
 	#installvmwareguest;			
@@ -19,7 +19,7 @@ defaultinstall.sh () {
 	#installspotify;
 	#installptf;
 	#installwine;
-	installsumatrapdf;
+	#installsumatrapdf;
 	#installvmware;
 	#installsublime;
 	#installhd;
@@ -57,14 +57,78 @@ installpentest () {
 
 installmobilepentest () {
 	update;
+
+	#install Android Studio
+	# only works atm if only one jdk package in downloads
+	DOWNLOADS=$HOME/Downloads
+	CURRENT_VERSION=$(java -version 2>&1)
+	JDK=` ls $DOWNLOADS | grep "jdk"`
+	STUDIO_URL=http://dl.google.com/android/studio/install/0.3.2/android-studio-bundle-132.893413-linux.tgz
+	if [ "uname -m" == "i386" -o "uname -m" == "i686" ]; then
+	    ARCH=32
+	else
+	    ARCH=64
+	fi
+
+	if [ "$ARCH" == "32" ]; then
+	    if [ "$JDK" == "" ]; then
+		(cd $DOWNLOADS; wget http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-linux-i586.tar.gz)
+	    fi
+	elif [ "$ARCH" == "64" ]; then
+	    if [ "$JDK" == "" ]; then
+		(cd $DOWNLOADS; wget http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-linux-x64.tar.gz)
+	    fi
+	else 
+	    echo "Architecture unable to be determined"
+	    exit
+	fi
+
+	# reset JDK variable
+	JDK=` ls $DOWNLOADS | grep "jdk"`
+
+	sudo apt-get install lib32ncurses5 ia32-libs 2>&1
+	tar xzvf $DOWNLOADS/$JDK
+	JDK_VERSION=` ls | grep "jdk" `
+	sudo mv $JDK_VERSION /usr/lib/jvm
+	sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/$JDK_VERSION/bin/java
+	sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/$JDK_VERSION/bin/javac
+	sudo update-alternatives --install /usr/bin/javaws javaws /usr/lib/jvm/$JDK_VERSION/bin/javaws
+	sudo update-alternatives --config java
+	sudo update-alternatives --config javaws
+
+	if [ ! -d $HOME/android-studio -o ! -f $DOWNLOADS/android-studio* ]; then
+	    (cd $DOWNLOADS; wget $STUDIO_URL)
+	    #verify download finished
+	    if [ ! -f $DOWNLOADS/android-studio* ]; then
+		echo "tar not found. Download failed?"
+		exit
+	    fi
+	fi
+	STUDIO_TAR=` ls $DOWNLOADS | grep "android-studio" `
+	tar xzvf $DOWNLOADS/$STUDIO_TAR
+	STUDIO_DIR=` ls | grep "android-studio" `
+	./$STUDIO_DIR/bin/studio.sh
+
+	#install useful tools
 	sudo apt install -y android-apktool androick android-sdk android-sdk-platform-tools androidpincrack androidsniffer androwarn androbugs androguard apkstudio backdoor-apk backhack dex2jar drozer kwetza lazydroid androbugs
 	
+	#install apktool
 	cd /opt/
 	mkdir android
 	cd android
 	git clone git://github.com/iBotPeaches/apktool.git
 	cd apktool
 	./gradlew build shadowJar proguard 
+
+
+	#install qark
+	apt install python3-ip
+	pip3 install --user qark
+
+	ln -s /root/.local/bin/qark /usr/local/bin/qark
+
+	echo "The qark version is: "
+	qark --version
 
 }
 
