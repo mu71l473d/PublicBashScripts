1c046ebc6f5e971f6a7abeccd35b24dcc0e934a5
mu71l473d <root@kali-xbto.bisperink.com>
2020-05-20 00:53:03 +0200

    new version

--- defaultinstall.sh
+++ defaultinstall.sh
@@ -8,8 +8,8 @@ defaultinstall.sh () {
 	#update;
 	#installfromapt;
 	#installfromaptgui;
-	installpentest;
-	installmobilepentest;
+	#installpentest;
+	#installmobilepentest;
 	#installicspentest;
 	#installvbguest;
 	#installvmwareguest;			
@@ -19,8 +19,8 @@ defaultinstall.sh () {
 	#installspotify;
 	#installptf;
 	#installwine;
-	installsumatrapdf;
-	#installvmware;
+	#installsumatrapdf;
+	installvmware;
 	#installsublime;
 	#installhd;
 	#installffdev;
@@ -49,7 +49,7 @@ installfromaptgui () {
 
 installpentest () {
 	update;
-	sudo apt install -y exiftool wine64 gdb wireshark seclists gobuster ftp php-curl python-smb mingw-w64
+	sudo apt install -y exiftool wine64 gdb tmux wireshark seclists gobuster ftp php-curl python-smb mingw-w64
 	if [ -n "$(uname -a | grep Kali)"]; then
 	sudo apt install kali-linux-everything -y
 	fi
@@ -180,10 +180,10 @@ installsignal () {
 
 installspotify () {
 	# 1. Add the Spotify repository signing keys to be able to verify downloaded packages
-	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
-
-	# 2. Add the Spotify repository
-	echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+	curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+	
+	# 2. add the repository to the sources.list
+	echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
 	# 3. Update list of available packages
 	update;
@@ -231,7 +231,7 @@ installgrub () {
 
 installvmware () {
    	cd ~/Downloads
-   	wget -O ~/vmware.bin https://www.vmware.com/go/getworkstation-linux
+   	wget -O vmware.bin https://www.vmware.com/go/getworkstation-linux
     	sudo apt install build-essential
     	sudo apt --fix-broken install
     	sudo bash ~/Downloads/vmware.bin
