f9cbda787d84027d15a1dcb00209a29521d90305
mu71l473d <judoka_rene@hotmail.com>
2020-08-08 18:45:18 +0200

    new version

--- newrepository.sh
+++ newrepository.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-repoName="$1"
+repoName=$1
+mkdir -p ~/github/$repoName
 
+test -z $repoName && echo "Repo name required." 1>&2
 
-echo "# $1" >> README.md
+curl -u 'mu71l473d' https://api.github.com/user/repos -d "{\"name\":\"$repoName\"}"
+
+echo "# $repoName" >> README.md
 git init
 git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/mu71l473d/$1.git
+git commit -m "first commit for $repoName"
+git remote add origin https://github.com/mu71l473d/$repoName.git
 git push -u origin master
